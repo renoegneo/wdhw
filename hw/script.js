78f6a59e3e2fefb081fb14dd909afac0668f3e1c
renoegneo <164788456+renoegneo@users.noreply.github.com>
2024-10-27 21:55:40 +0600

    Add files via upload

---
+++ hw/script.js
@@ -0,0 +1,42 @@
+// let age = prompt("Age: ");
+// if (age > 100 || age < 0){
+//     alert("ne vri")
+// }
+// else{
+//     if (age >=85){
+//         alert("Exc")
+//     }
+//     else if (age >=65){
+//         alert("good")
+//     }
+//     else if (age >=40){
+//         alert("Avg")
+//     }
+//     else{
+//         alert("fail")
+//     }
+// // }
+// let login = "123";
+// let passw = "1234";
+// let ulog;
+// let upass;
+// function signin() {
+//     ulog = (prompt("Enter login"));
+//     upass = (prompt("enter password"));
+//     if (ulog.toLowerCase == login && upass.toLowerCase == passw){
+//         alert("Successful login");
+//     }
+//     else{
+//         alert("Uncorrect login or password");
+//     }
+// }
+// signin();
+
+// const number = parseInt(prompt("Введите число для вычисления факториала:"));
+// let factorial = 1;
+
+// for (let i = 1; i <= number; i++) {
+//     factorial *= i;
+// }
+// alert(`Факториал числа ${number} равен ${factorial}`);
+
